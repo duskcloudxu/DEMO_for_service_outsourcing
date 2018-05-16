61c481407e8f9d1e2ed714dedfe2cc2175bc2420
duskcloudxu <duskcloudxu@gmail.com>
2018-05-16 09:28:29 +0800

    project structure completed, slides tested.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,9 @@ import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+import { TabsPage } from '../pages/tabs/tabs'
+import { LoginPage } from '../pages/login/login';
+import { ContractPage } from '../pages/contract/contract';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -17,11 +19,17 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    LoginPage,
+    ContractPage,
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp,{
+      tabsHideOnSubPages: 'true',         //ionic3隐藏全部子页面tabs
+      mode: 'md'          //把所有平台设置为iOS风格：
+    })
+
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -29,7 +37,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    LoginPage,
+    ContractPage,
   ],
   providers: [
     StatusBar,
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,17 +3,36 @@ import { Component } from '@angular/core';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import { ContractPage } from '../contract/contract';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
-
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tabRoots: Object[];
 
   constructor() {
-
+    this.tabRoots = [
+      {
+        root: HomePage,
+        tabTitle: '首页',
+        tabIcon: 'home'
+      },
+      {
+        root: ContactPage,
+        tabTitle: '会话',
+        tabIcon: 'notifications'
+      },
+      {
+        root: ContractPage,
+        tabTitle: '合同',
+        tabIcon: 'document'
+      },
+      {
+        root: AboutPage,
+        tabTitle: '我的',
+        tabIcon: 'person'
+      }
+    ];
   }
 }
